6f172f787fda91b85179c196de641d933785f6ce
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2024-01-12 22:58:32 +0100

    at least it loads now

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import CMS from '@staticcms/core';
 import '@staticcms/core/dist/main.css';
-import YAML from 'yaml';
 
-// fetch /config.yaml and parse
-fetch('/config.yaml').then((res) => res.text()).then((yaml) => {
-  const config = YAML.parse(yaml);
-  console.log(config);
-});
+// Initialize Static CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
+// CMS.registerPreviewTemplate('my-template', MyTemplate);
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import yaml from '@rollup/plugin-yaml'
-
-export default defineConfig({
-  plugins:[
-    yaml(),
-  ]
-})
